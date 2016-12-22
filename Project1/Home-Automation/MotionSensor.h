c58bcf46ecb4d534518e77bf695e4233f6919d69
Niels <niels_shadow@hotmail.com>
2016-12-22 17:01:08 +0100

    Update and rename MotieSensor.h to MotionSensor.h

--- Project1/Home-Automation/MotionSensor.h
+++ Project1/Home-Automation/MotionSensor.h
@@ -4,13 +4,14 @@
 #include "Lamp.h"
 using namespace std;
 
-class MotieSensor :public Sensor{
+class MotionSensor :public Sensor{
 public:
-	MotieSensor(int id,Lamp &gebondenlicht);
+	MotionSensor(int id,Lamp &gebondenlicht);
 	void setlicht();		
 	bool check();
 private:	
 	Lamp& gebondenlicht;
+	int id;
 };
 #endif
 
