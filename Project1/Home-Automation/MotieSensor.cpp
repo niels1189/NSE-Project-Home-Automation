d4b2116b484b896ea64d85343db9d5b585da47b9
Niels <niels_shadow@hotmail.com>
2016-12-22 17:06:56 +0100

    Update MotieSensor.cpp

--- Project1/Home-Automation/MotieSensor.cpp
+++ Project1/Home-Automation/MotieSensor.cpp
@@ -1,18 +1,22 @@
-#include "MotieSensor.h"
+#include "MotionSensor.h"
 #include "Lamp.h"
+#include <time.h>
 
-MotieSensor::MotieSensor(int id,Lamp&gebondenlicht):gebondenlicht(gebondenlicht), Sensor(id, true) {
-	
+MotionSensor::MotieSensor(int id,Lamp&gebondenlicht):gebondenlicht(gebondenlicht), Sensor(id, true){
+	if(0<id<4)
+		this->id = id;
+	else this->id = 0;
 }
 
-bool MotieSensor::check() {	
-	if (alive()) {
+bool MotieSensor::check() {
+	if(this->detectMotion()) {
+		gebondenlicht.Activeer(this->id);
+		setactief(int id);
+		time(&this->expire);
+		// difftime(timenew,this->expire); // timenew-this->expire
+		return true;
+	} else {
+		Guard.Notify();
 		return false;
 	}
-	return true;
-}
-
-void MotieSensor::setlicht() {
-	setactief();
-	gebondenlicht.Activeer();
 }
