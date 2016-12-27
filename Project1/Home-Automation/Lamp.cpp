5b36e5420794645071927a903ed232e48501fa52
Niels <niels_shadow@hotmail.com>
2016-12-27 12:50:15 +0100

    Lamp Update

--- Project1/Home-Automation/Lamp.cpp
+++ Project1/Home-Automation/Lamp.cpp
@@ -4,23 +4,28 @@
 
 Lamp::Lamp() {
 	actief = 0;
+	timeout = 900; // After how many seconds will the light deactivate itself?
 
 }
 
-void Lamp::Activeer(void) {
-	actief = true;
-	start = std::clock();
+Lamp::Lamp(int timeout) {
+	actief = 0;
+	this.timeout = timeout; // After how many seconds will the light deactivate itself?
+
 }
 
-void Lamp::Deactiveer() {
-	start = 0;
+void Lamp::Activate() {
+	active = true;
+	expire = std::clock()+this.timeout;
+}
+
+void Lamp::Deactivate() {
+	expire = 0;
 	actief = 0;
 }
 
-void Lamp::check() {
-	if (start > 0) {
-		long x = std::clock() - start;
-		if (x > (1500 * 60))
+void Lamp::Check() {
+	if (std::clock() > expire) {
 			Deactiveer();
 	}
 }
