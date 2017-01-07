e7bf076c94c91cd6454be4d1c3e13a26cde206e1
Niels <niels_shadow@hotmail.com>
2017-01-07 21:21:27 +0100

    Update MotionSensor.h

--- Sensor+I2C+GPIO+Log+GUI/MotionSensor.h
+++ Sensor+I2C+GPIO+Log+GUI/MotionSensor.h
@@ -5,11 +5,14 @@
 #include "Light.h"
 
 class MotionSensor:public Sensor{
-public:
-MotionSensor(int id,I2CCom& i2c,Camera& cam,Light& light);
-bool Check();
-private:
-Light& light;
+  public:
+    MotionSensor(int id,I2CCom& i2c,Camera& cam,Light& light);
+    bool Check();
+  private:
+    Light& light;
+    int Active        = 0;
+    int Timer         = 0;
+    const int TimeOut = 300;  // How long will it take for the motionsensor to die
 };
 
 #endif
