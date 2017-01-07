5646a638d63294d087335ea95d977d5e3198d3c0
Niels <niels_shadow@hotmail.com>
2017-01-07 20:16:40 +0100

    Update Light.h

--- Sensor+I2C+GPIO+Log+GUI/Light.h
+++ Sensor+I2C+GPIO+Log+GUI/Light.h
@@ -2,19 +2,23 @@
 #define LIGHT_H
 
 #include <thread>
+/* Program runs on the principle of a dead-timer
+   Once the dead timer is reached, the lights turn off
+   Once motion is detected, the timers will update itself.   
+*/
 
 class Light{
-public:
-Light(int pin);
-bool Check();
-void Set_Light(bool x);
-private:	
-int Timer =0 ;	
-const int TimeOut = 900;
-bool Active = false;
-//int Value = 0;
-int Pin;
-
+	public:
+		Light(int pin);
+        void TurnOn(int pin);
+        void TurnOff(int pin);
+        bool Check();                   // Routine check
+        void Set_Light(bool x);
+    private:	
+        int Timer           = 0; 
+        const int TimeOut   = 900;      // How long will it take for the lights to turn off automatically
+        bool Active         = false;    // Lights are off by default
+        int Pin;
 };
 
 #endif
