6a7b21fbf53b174902f31039509643d48a7d57ac
Niels <niels_shadow@hotmail.com>
2017-01-09 15:41:06 +0100

    Update Sensor.cpp

--- Sensor+I2C+GPIO+Log+GUI/Sensor.cpp
+++ Sensor+I2C+GPIO+Log+GUI/Sensor.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-Sensor::Sensor(int id,I2CCom& x,Camera& cam):ID(id),Timer(time(0)+TimeOut),i2c(x),camera(cam){}
+Sensor::Sensor(int id,I2CCom& x):ID(id),Timer(time(0)+TimeOut),i2c(x){}
 
 int Sensor::GetValue() {
 	return i2c.I2C_GetValue(ID);
