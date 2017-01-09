4b9b12e2bbdef5b874541b70cd862fb49ec4c432
Niels <niels_shadow@hotmail.com>
2017-01-09 15:42:18 +0100

    Update MotionSensor.cpp

--- Sensor+I2C+GPIO+Log+GUI/MotionSensor.cpp
+++ Sensor+I2C+GPIO+Log+GUI/MotionSensor.cpp
@@ -1,26 +1,14 @@
 #include "Sensor.h"
 #include "MotionSensor.h"
 
-
-MotionSensor::MotionSensor(int id,I2CCom& i2c,Camera& cam,Light& light):Sensor(id,i2c,cam),light(light){}
-
-
-bool MotionSensor::Check(){
-	if(i2c.I2C_GetValue()>0){
-		this->Active=1; // Motion sensor is active
-		Timer = time(0) + TimeOut;
-		light.Set_Light(true); // Turn on the bound lights
+bool MotionSensor::Check(){	
+	if(GetValue()>0) { // Motion sensor is active
+		SetActive(); // Set dead-timer
 		return true;
 	}
-	
-	if((time(0) > Timer)&&(Active)){ // if the current time is less than the Timer (for expiry)
-		this->Active=false; //(meaning the dead-timer for the motionsensor has expired)
-		return 0; // Return 0 because the timer expired.
-	}
-	else if(Active) {
-		return 1; // If motionsensor is still active but not expired, return a 1
+	else if(Alive()) { // If motionsensor is still active but not expired
+		return true;
 	}
 	else
-		return 0; // If the motionsensor is inactive, return 0
-	return true;
+		return false;
 }
