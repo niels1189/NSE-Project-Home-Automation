218ab7031e6a15a117fd947d6ca0e09c13afac47
Niels <niels_shadow@hotmail.com>
2017-01-09 15:44:39 +0100

    Update Pressuresensor.h

--- Sensor+I2C+GPIO+Log+GUI/Pressuresensor.h
+++ Sensor+I2C+GPIO+Log+GUI/Pressuresensor.h
@@ -7,7 +7,7 @@
 
 class PressureSensor : public Sensor{
 public:
-	PressureSensor(int id,I2CCom& x,Camera& cam,Log& log);
+	PressureSensor(int id,I2CCom& x,Log& log);
 	bool Check();
 private:
 	int value;		
