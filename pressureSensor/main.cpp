bd083bb82a70870de20938c7b89a421aa9b9c20f
NRamdat <niteshvganesh@gmail.com>
2017-01-12 15:29:27 +0100

    Update

--- pressureSensor/main.cpp
+++ pressureSensor/main.cpp
@@ -6,16 +6,16 @@ int main(void) {
 	while(1) {
 		int value = b.value();
 		if(value < 20) { // Nothing on the bed or just some book. 
-			//Do something
+			//Do nothing just log
 		}
 		if(value > 20 && value < 150) { // Maybe an attack
-			//Do something
+			//Switch camera on
 		}
 		if(value > 150 && value < 200) { // Changing positions while asleep
-			//Do something
+			//Do nothing, maybe verify if person really is sleeping
 		}
 		if(value > 200) { // Stable, person is sleeping
-			//Do something
+			//DO nothing just log 
 		}
 		
 	}
