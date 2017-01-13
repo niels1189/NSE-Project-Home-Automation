d5a3cb99a120851fcb7db6d0b335734eb800d7df
T-Koot <thijskoot@outlook.com>
2017-01-13 15:32:54 +0100

    comments added

--- Sensor+I2C+GPIO+Log+GUI/Log.h
+++ Sensor+I2C+GPIO+Log+GUI/Log.h
@@ -1,14 +1,14 @@
 #ifndef LOGFILE_H
 #define LOGFILE_H
-#include <string>
+//#include <string>
 class Log{
 private:
-	const char* loc;
+	const char* loc;  //the name of the file to write in
 public:
-	void Write(std::string text);
-	void LogInBed();
-	void LogOutBed();
-	Log(const char* loc);	
+	void Write(std::string text);   //write to given file
+	void LogInBed();		//write specific text | when going in bed
+	void LogOutBed();		//write specific text | when getting out of bed
+	Log(const char* loc);		//constructor
 
 };
 
