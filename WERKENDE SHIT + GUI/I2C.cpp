a5fe81c1e4edc5d6145b459861fb9561c8244781
NRamdat <niteshvganesh@gmail.com>
2017-01-20 15:26:22 +0100

    Update I2C.cpp

--- WERKENDE SHIT + GUI/I2C.cpp
+++ WERKENDE SHIT + GUI/I2C.cpp
@@ -10,25 +10,33 @@
 #include<linux/i2c.h>
 #include<linux/i2c-dev.h>
 
-#include "I22cCom.h"
+#include "I2C.h"
 using namespace std;
 
 
-I2CCom::I2CCom(const char* File) {
+I2CCom::I2CCom(const char* File,int address) {
 	int file = open(File, O_RDWR);
-	ioctl(file, I2C_SLAVE, 0x08);
+	ioctl(file, I2C_SLAVE, address);
 	i2cfile = file;
 }
 
 int I2CCom::I2C_GetValue(int hex) {
+for(int x=0;x<2;x++){ //Called thrice to get rid of potential overflow/static
 	int writebuffer[10];
 	writebuffer[0] = hex;
 	write(i2cfile, writebuffer, 1);
 
 	int readbuffer[10];
 	read(i2cfile, readbuffer, 1);
-	cout << hex << ":  " << readbuffer[0] << endl;
+}	
+//	int writebuffer[10];
+//	writebuffer[0] = hex;
+//	write(i2cfile, writebuffer, 1);
 	
+	int readbuffer[10]={0};
+	read(i2cfile, readbuffer, 1);
+	//readbuffer[0]-=85504;
+//	cout<<readbuffer[0]<<endl;
 	return readbuffer[0];
 }
 
