05775e4f685fced562a45c6ffd1b1efdc64e4472
NRamdat <niteshvganesh@gmail.com>
2017-01-20 13:13:41 +0100

    Bug fix.

--- Master/Main.cpp
+++ Master/Main.cpp
@@ -31,13 +31,14 @@ vector<Light*> lights;      //Vector of the lights
 vector<int> active;
 PressureSensor* pressureSensor;
 Camera* cam;                //Pointer to the camera
-Log* log;
+//Log* log;
 
 int pressureValue;
 bool asleep = false;
 bool day = true;
 bool anomaly = false;
 int temperature = 20;
+int IngesteldeTemperatuur = 20;
 long sleepTimer = 0;
 
 
@@ -79,12 +80,12 @@ void init() {
 	MotionSensor s2(0xBC,i2c);
 	MotionSensor s3(0xEC,i2c);
 	PressureSensor s4(0x06, i2c);
-	Log l1(LOG);
+	//Log l1(LOG);
 	Camera c1;
     
     //Create pointers to the sensors
 	cam = &c1;
-	log = &l1;
+	//log = &l1;
 	pressureSensor = &s4;
 	motionSensors.push_back(&s1);
 	motionSensors.push_back(&s2);
@@ -97,22 +98,21 @@ void init() {
 void updateSensors() {
     //update van elke sensor de value en de active
     bool alert = true;
-    for(int i = 0; i<motionSensors.size();i++) {
+    for(unsigned int i = 0; i<motionSensors.size(); ++i) {
         if(motionSensors[i]->check()) {
             //active[i]=1;
             alert = false;
-		cout <<"halleyula" <<endl;
         } else{
             //active[i]=0;
         }
     }
-    if (s4->check()) {
+    if (pressureSensor->check()) {
         asleep = true;
     }
     if(alert & !asleep) {
         sendAlert();
     }
-    pressureValue = s4->getValue();
+    pressureValue = pressureSensor->getValue();
 }
 
 /*Send Alarm*/
@@ -143,18 +143,18 @@ void checkSleep(){
         sleepTimer = 0;
         // Changing positions while asleep
         //Do nothing, maybe verify if person really is sleeping
-    } else if(pressureValue > 200 && sleepTimer = 0) {
+    } else if(pressureValue > 200 && sleepTimer == 0) {
         sleepTimer = time(0) + 900;
     } else if(pressureValue >200 && sleepTimer != 0) {
         if( time(0) >= sleepTimer) {
-            asleep = true
+            asleep = true;
         }
     }
     
 }
 
 /*Checks the set temperature from the gui*/
-checkTemperature() {
+void checkTemperature() {
     temperature = IngesteldeTemperatuur;
 }
 
